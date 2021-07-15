98771b50887707a00cbadfcd5972226c21078630
minsuk <palt1230@naver.com>
2021-07-15 18:08:09 +0900

    catalog service 추가

---
+++ msa-ecommerce/catalog-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('first', 'cookie', 100, 10);
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('second', 'coke', 100, 10);
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('third', 'sprite', 100, 20);
