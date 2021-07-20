3e1983f83e36e201f670d56492d432e3c59ee094
minsuk <palt1230@naver.com>
2021-07-20 20:55:36 +0900

    config server 재추가

--- msa-ecommerce/config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
+++ msa-ecommerce/config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
@@ -2,7 +2,11 @@ package com.example.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.cloud.config.server.EnableConfigServer;
+
+import java.util.Enumeration;
+import java.util.Properties;
 
 @SpringBootApplication
 @EnableConfigServer
